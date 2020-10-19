328c23a46cbea8393835a70bb0a8a02b06e12628
Rami Alrwais <raalrwai@ucsc.edu>
2020-10-19 16:10:47 -0700

    switch to port 80

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,8 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 require('dotenv').config();
 const app = express();
-const port = process.env.PORT || 5000;
+
+const port = process.env.PORT || 80;
 app.use(cors());
 app.use(express.json());
 const uri = process.env.ATLAS_URI;
